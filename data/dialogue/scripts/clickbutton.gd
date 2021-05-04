8961d2ede8fecd92913e1598822ba0de3b152720
CultOfApe <ted.project.bcn@gmail.com>
2021-05-04 17:58:37 +0200

    Further tidying up of folder/file structure

---
+++ data/dialogue/scripts/clickbutton.gd
@@ -0,0 +1,26 @@
+extends Label
+
+signal on_click(a, b, c)
+signal on_hover(a)
+
+var id : String 
+var branch : String 
+
+func _ready():
+	pass
+
+func _on_button_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.is_pressed():
+			emit_signal("on_click", id, branch, true, 0)
+		else:
+			pass
+
+func _on_button_mouse_entered():
+	add_color_override("font_color", Color(1,0,1))
+	emit_signal("on_hover",true)
+
+
+func _on_button_mouse_exited():
+	add_color_override("font_color", Color(1,1,1))
+	emit_signal("on_hover",false)
