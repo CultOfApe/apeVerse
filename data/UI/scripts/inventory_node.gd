17fb0e31fc68424968ee201ac060c1446861beed
CultOfApe <ted.project.bcn@gmail.com>
2021-07-31 12:04:46 +0200

    Simplified inventory node code
    what the title says

--- data/UI/scripts/inventory_node.gd
+++ data/UI/scripts/inventory_node.gd
@@ -1,28 +1,17 @@
 extends Sprite
 
 var id = null
-var hovering = false
 
 signal change_cursor(a, b)
 
 func _ready():
 	var ui_node = get_node("/root/world/ui")
-#	ui_node.connect("change_cursor", self, "item_in_hand")
 
-func _process(delta):
-	pass
-
-func _input(event):
-	if hovering == true:
-		if event is InputEventMouseButton:
-			if event.button_index == BUTTON_LEFT:
-				if event.pressed:
-					global.itemInHand = id
-					global.sceneCol.disabled = true
-					emit_signal("change_cursor", "hand", "res://data/ui/graphics/inv_" + id + ".png")
-			
-func _on_area_mouse_entered():
-	hovering = true
-
-func _on_area_mouse_exited():
-	hovering = false
+func _on_area_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.pressed:
+				global.itemInHand = id
+				global.sceneCol.disabled = true
+				global.blocking_ui = true
+				emit_signal("change_cursor", "hand", "res://data/ui/graphics/inv_" + id + ".png")
